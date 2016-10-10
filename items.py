ec7063fc7b08fe30a71a6cae900c8c48be1c1cf2
KBarrett-Powell <Barrett-PowellK@cardiff.ac.uk>
2016-10-10 22:14:02 +0100

    added basic outline to item dictionary

---
+++ items.py
@@ -0,0 +1,16 @@
+item_cricketbat{"name": "Cricket Bat",
+"description": """
+""",
+"health": 4,
+"weapon": True,
+}
+
+item_shotgun{}
+
+item_bottle{}
+
+item_needle{}
+
+item_condom{}
+
+item_coffee{}
