29e7ed783489da43d58f4290766cc0e58fd91840
James Hodgson <hodgyj@outlook.com>
2016-10-31 17:41:56 +0000

    Wording

--- chats.py
+++ chats.py
@@ -1,8 +1,8 @@
 chats = [
     "You chat for a while. How cute",
     "You find out that you both share some common interests.\nWho'd have thought?",
-    "You try talking, but the clown doesn't respond.",
-    "You confess your attraction to the clown. They suddenly get really awkward.",
+    "You try talking, but they don't respond.",
+    "You confess your attraction to them. They suddenly get really awkward.",
     "You try talking about football. It was bad. At least you tried"
 ]
 
